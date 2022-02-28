4bc95a2039bb30d0aa70172c5d57759de53abe40
Corentin Regal <corentin@huggingface.co>
2022-02-28 14:24:01 +0100

    Add resource data stream

--- es/provider.go
+++ es/provider.go
@@ -80,6 +80,7 @@ func Provider() *schema.Provider {
 			"elasticsearch_snapshot_repository":       resourceElasticsearchSnapshotRepository(),
 			"elasticsearch_snapshot_lifecycle_policy": resourceElasticsearchSnapshotLifecyclePolicy(),
 			"elasticsearch_watcher":                   resourceElasticsearchWatcher(),
+			"elasticsearch_data_stream":               resourceElasticsearchDataStream(),
 		},
 
 		ConfigureFunc: providerConfigure,
